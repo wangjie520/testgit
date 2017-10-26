555f89b3bb808c1fa92ee06b379065581bc8333c
wangjie <1126775097@qq.com>
2017-10-26 18:30:50 +0800

    delete xxx

--- VCMuiTest/bin/classes.dex.d
+++
@@ -1,7 +0,0 @@
-D:\adt-bundle-windows-x86-20140702\workspace\VCMuiTest\bin\classes.dex : \
-D:\adt-bundle-windows-x86-20140702\workspace\VCMuiTest\bin\classes\com\checheyun\vcm\VcmUiTest\AllCaseTestEv.class \
-D:\adt-bundle-windows-x86-20140702\workspace\VCMuiTest\bin\classes\com\checheyun\vcm\VcmUiTest\loggin.class \
-D:\adt-bundle-windows-x86-20140702\workspace\VCMuiTest\bin\classes\com\checheyun\vcm\VcmUiTest\StaticO.class \
-D:\adt-bundle-windows-x86-20140702\workspace\VCMuiTest\bin\classes\com\checheyun\vcm\VcmUiTest\UiAutomatorHelper.class \
-D:\adt-bundle-windows-x86-20140702\workspace\VCMuiTest\bin\classes\com\checheyun\vcm\VcmUiTest\UItestAllCases.class \
-D:\adt-bundle-windows-x86-20140702\workspace\VCMuiTest\bin\classes\com\checheyun\vcm\VcmUiTest\VCMuitestOutputRpt.class \
